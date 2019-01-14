66bb959a2f3b12d8a294f59a9298ca21c2031dc6
Jack Kelliher <jke0429@stacmail.net>
2019-01-14 16:24:52 +1300

    adding votes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ var app = new Vue({
   el: '.content',
 
   data: {
-    movies: []
+    movies: {},
+    people: [],
+    user: 1
   },
   created: function() {
     this.fetchData();
@@ -11,11 +13,53 @@ var app = new Vue({
     fetchData: function() {
       var self = this;
       var list = 'data.json';
+      var votes = 'data-votes.json';
+      var people = 'data-people.json';
+
+
+      fetch(people).then(function(peopleResponse){
+        return peopleResponse.json();
+      }).then(function(peopleJson) {
+        self.people = peopleJson.people;
+      });
 
       fetch(list).then(function(response) {
         return response.json();
       }).then(function(json) {
-        self.movies = json.movies;
+        var movies = {}
+
+        json.movies.forEach(function(movie){
+          movie.votes = 0
+          movies[movie.id] = movie
+        });
+
+        self.movies = movies;
+
+        fetch(votes).then(function(voteResponse) {
+          return voteResponse.json();
+        }).then(function(voteJson) {
+          voteJson.votes.forEach(function(vote) {
+            var id = vote.movie.id;
+            var movie = self.movies[id];
+
+            Vue.set(movie, 'votes', movie.votes + 1);
+          });
+        });
+      });
+    },
+    sendVote: function(id) {
+      var self = this;
+      var voteEndpoint = 'data-votes.json';
+
+      fetch(
+        voteEndpoint + self.user + '/' + id,
+        { method: 'POST'}
+      ).then(function(response){
+        if (response.ok) {
+          Vue.set(self.movies[id], 'votes', self.movies[id].votes + 1);
+        } else {
+          alert('You have already voted for this!');
+        }
       });
     }
   }
