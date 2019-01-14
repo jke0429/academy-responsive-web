31789641fef35c30c54dd29f54b62159b30aca2c
Jack Kelliher <jke0429@stacmail.net>
2019-01-14 14:30:03 +1300

    List the movies

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-$(document).ready(function() {
+var app = new Vue({
+  el: '.content',
 
-  // Your code goes in here    
+  data: {
+    movies: []
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: function() {
+      var self = this;
+      var list = 'data.json';
 
+      fetch(list).then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        self.movies = json.movies;
+      });
+    }
+  }
 });
